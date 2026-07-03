a27fde4f50db737471950f6c6e2d72b81cd0bf76
ManishPuraswani <mpuraswaniphd@gmail.com>
2026-07-03 16:55:04 +0530

    Refactor Binarize forward method for binary operations
    Refactor forward method to include binary input and weight handling.

--- models/binarized_modules_imc.py
+++ models/binarized_modules_imc.py
@@ -12,20 +12,58 @@ import numpy as np
 
 class Binarize(InplaceFunction):
 
-    def forward(ctx,input,quant_mode='det',allow_scale=False,inplace=False):
-        ctx.inplace = inplace
-        if ctx.inplace:
-            ctx.mark_dirty(input)
-            output = input
-        else:
-            output = input.clone()      
+   def forward(self, input):
 
-        scale= output.abs().max() if allow_scale else 1
+    # --------------------------------------------------
+    # Save original floating-point input
+    # --------------------------------------------------
+    self.input_float = input.detach().cpu().clone()
 
-        if quant_mode=='det':
-            return output.div(scale).sign().mul(scale)
-        else:
-            return output.div(scale).add_(1).div_(2).add_(torch.rand(output.size()).add(-0.5)).clamp_(0,1).round().mul_(2).add_(-1).mul(scale)
+    # --------------------------------------------------
+    # Binary activation
+    # --------------------------------------------------
+    if input.size(1) != 3:
+        input_b = binarized(input)
+    else:
+        input_b = input
+
+    self.input_binary = input_b.detach().cpu().clone()
+
+    # --------------------------------------------------
+    # Save original floating-point weights
+    # --------------------------------------------------
+    self.weight_float = self.weight.detach().cpu().clone()
+
+    # --------------------------------------------------
+    # Binary weights
+    # --------------------------------------------------
+    weight_b = binarized(self.weight)
+
+    self.weight_binary = weight_b.detach().cpu().clone()
+
+    # --------------------------------------------------
+    # Convolution
+    # --------------------------------------------------
+    out = nn.functional.conv2d(
+        input_b,
+        weight_b,
+        None,
+        self.stride,
+        self.padding,
+        self.dilation,
+        self.groups
+    )
+
+    if self.bias is not None:
+        self.bias.org = self.bias.data.clone()
+        out += self.bias.view(1, -1, 1, 1).expand_as(out)
+
+    # --------------------------------------------------
+    # Save floating-point output
+    # --------------------------------------------------
+    self.output_float = out.detach().cpu().clone()
+
+    return out
         
     def backward(ctx,grad_output):
         #STE 
